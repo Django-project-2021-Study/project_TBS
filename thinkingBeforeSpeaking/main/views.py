74c0910f702aa1ee65c3fe80a9b4e6078ba79d29
LeeJongKeun <johnny550@naver.com>
2021-08-21 14:48:38 +0900

    오라가짜

--- thinkingBeforeSpeaking/main/views.py
+++ thinkingBeforeSpeaking/main/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    return render(request, 'ghostwriting.html')
+
+def detail(request):
+    return render(request, 'detail.html')
+
+def board(request):
+    return render(request, 'board.html')
--- thinkingBeforeSpeaking/thinkingBeforeSpeaking/urls.py
+++ thinkingBeforeSpeaking/thinkingBeforeSpeaking/urls.py
@@ -15,7 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from main.views import *
+from accounts.views import *
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('', home, name="home"),
+    path('board', board, name="board"),
+    path('detail', detail, name="detail"),
+    path('login', login, name="login"),
+    path('signup', signup, name="signup"),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
